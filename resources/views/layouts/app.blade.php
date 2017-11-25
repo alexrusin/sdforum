ffa536b8f71ebbbb5187a1c6b06444a15afcfb79
Alex Rusin <alexrusin_2000@yahoo.com>
2017-11-25 10:03:52 -0800

    add all threads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
                         <li><a href="{{ route('threads') }}">All Threads</a></li>
 
                         <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Channels<span class="caret"></span></a>
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">All Channels<span class="caret"></span></a>
                           <ul class="dropdown-menu">
                             @foreach (App\Channel::get() as $channel)
                                 <li><a href="{{route('channel', ['channel'=> $channel->slug])}}">{{ucfirst($channel->name)}}</a></li>
