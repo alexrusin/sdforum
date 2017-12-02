bfbcd8db567900680aae74e69607acdf7f74e204
Alex Rusin <alexrusin_2000@yahoo.com>
2017-12-01 22:49:38 -0800

    meta details and pagination

--- app/Thread.php
+++ app/Thread.php
@@ -10,6 +10,15 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
 	protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyCount', function($builder){
+            $builder->withCount('replies');
+        });
+    }
 	
     public function path()
     {
@@ -22,6 +31,7 @@ class Thread extends Model
     {
     	return $this->hasMany(Reply::class);
     }
+
 
     public function creator()
     {
