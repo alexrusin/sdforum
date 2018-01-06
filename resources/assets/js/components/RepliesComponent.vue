0aead342701b738a2a135e89ab9c67cf1be27881
Alex Rusin <alexrusin_2000@yahoo.com>
2018-01-06 11:21:50 -0800

    new reply vue component.  moment js

--- resources/assets/js/components/RepliesComponent.vue
+++ resources/assets/js/components/RepliesComponent.vue
@@ -1,24 +1,32 @@
 <template>
     <div>
        <div v-for="(reply, index) in items">
-            <reply :data="reply" @deleted="remove(index)"></reply>
+            <reply :data="reply" @deleted="remove(index)" :key="reply.id"></reply>
        </div>
+
+       <new-reply :endpoint="endpoint" @created="add"></new-reply>
     </div>
 </template>
 
 <script>
     import Reply from './ReplyComponent.vue';
+    import NewReply from './NewReplyComponent.vue';
     export default {
         props: ['data'],
-        components: {Reply},
+        components: {Reply, NewReply},
        
         data() {
           return {
-            items: this.data
+            items: this.data,
+            endpoint: location.pathname + '/replies'
            }
         },
 
         methods: {
+            add(reply) {
+                this.items.push(reply);
+                this.$emit('added');
+            },
             remove(index) {
                 this.items.splice(index, 1);
                 this.$emit('removed');
--- resources/assets/js/components/ReplyComponent.vue
+++ resources/assets/js/components/ReplyComponent.vue
@@ -4,7 +4,7 @@
 			<div class="level">
 				<h5 class="flex">
 					<a :href="'/profiles/' + data.owner.name" v-text="data.owner.name">
-						</a> said {{data.created_at}}...
+						</a> said <span v-text="ago"></span>
 				</h5>
 				
 				<div v-if="signedIn">
@@ -41,6 +41,7 @@
 
 <script>
 	import Favorite from './FavoriteComponent.vue';
+	import moment from 'moment';
 	export default {
 		props: ['data'],
 		components: {Favorite},
@@ -59,6 +60,10 @@
 
 			canUpdate() {
 				return this.authorize(user => this.data.user_id == user.id);
+			},
+
+			ago() {
+				return moment(this.data.created_at + 'Z').fromNow();
 			}
 		},
 
