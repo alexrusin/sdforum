edc774fc54988ee61a7ea9add017b5b0dd4a0340
Alex <alexrusin_2000@yahoo.com>
2018-09-02 07:02:59 -0700

    Apply fixes from StyleCI (#5)

--- app/Channel.php
+++ app/Channel.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
-	protected $guarded = [];
-	
+    protected $guarded = [];
+
     public function getRouteKeyName()
     {
         return 'slug';
