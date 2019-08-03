cf5bf071b342c47a0bf0572a4f3b4cda32040a9b
Alex Rusin <alexrusin_2000@yahoo.com>
2019-08-03 09:21:34 -0700

    update analytics package

--- config/analytics.php
+++ config/analytics.php
@@ -3,7 +3,7 @@
 return [
     'api_token' => env('ANALYTICS_API_TOKEN'),
     'timezone' => env('ANALYTICS_TIMEZONE', 'America/Los_Angeles'),
-    'database_connection' => env('ANALYTICS_DB_CONNECTION', 'mysql'),
+    'database_connection' => env('ANALYTICS_DB_CONNECTION', env('DB_CONNECTION', 'mysql')),
     'path' => env('ANALYTICS_PATH', 'analytics'),
     'middleware_group' => env('ANALYTICS_MIDDLEWARE_GROUP', 'web'),
 ];
