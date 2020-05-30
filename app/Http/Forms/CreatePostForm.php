d3add7f5d2484f1ab43d8968249599a8de114686
Alex <alexrusin_2000@yahoo.com>
2020-05-30 01:04:21 +0000

    Apply fixes from StyleCI

--- app/Http/Forms/CreatePostForm.php
+++ app/Http/Forms/CreatePostForm.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Forms;
 
-use App\Reply;
-use Illuminate\Support\Facades\Gate;
 use App\Exceptions\ThrottleException;
+use App\Reply;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 
 class CreatePostForm extends FormRequest
 {
