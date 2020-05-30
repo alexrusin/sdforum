0d2f662c4a0092ba2600d0659777bb196add7ca6
Alex <alexrusin_2000@yahoo.com>
2020-05-30 01:05:33 +0000

    Apply fixes from StyleCI

--- database/factories/DatabaseNotificationFactory.php
+++ database/factories/DatabaseNotificationFactory.php
@@ -1,17 +1,17 @@
 <?php
 
-use App\User;
-use Ramsey\Uuid\Uuid;
-use Faker\Generator as Faker;
 use App\Notifications\ThreadWasUpdated;
+use App\User;
+use Faker\Generator as Faker;
 use Illuminate\Notifications\DatabaseNotification;
+use Ramsey\Uuid\Uuid;
 
 $factory->define(DatabaseNotification::class, function (Faker $faker) {
     return [
-       'id' => Uuid::uuid4()->toString(),
-       'type' => ThreadWasUpdated::class,
-       'notifiable_id' => auth()->id() ?: factory(User::class)->create()->id,
-       'notifiable_type' => User::class,
-       'data' => ['foo' => 'bar']
+        'id' => Uuid::uuid4()->toString(),
+        'type' => ThreadWasUpdated::class,
+        'notifiable_id' => auth()->id() ?: factory(User::class)->create()->id,
+        'notifiable_type' => User::class,
+        'data' => ['foo' => 'bar']
     ];
 });
